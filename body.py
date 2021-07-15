6c030e12e9f7e0886e86e5fe763cc7abea75f1b8
jianhaozhan <55318437+JianhaoZhan@users.noreply.github.com>
2021-07-15 15:57:19 +0800

    Add files via upload

---
+++ body.py
@@ -0,0 +1,22 @@
+import cv2
+import matplotlib.pyplot as plt
+import copy
+import numpy as np
+
+from src import model
+from src import util
+from src.body import Body
+
+body_estimation = Body('model/body_pose_model.pth')
+
+test_image = 'images/detect_hand_preview.jpg'
+oriImg = cv2.imread(test_image)  # B,G,R order
+candidate, subset = body_estimation(oriImg)
+#canvas = copy.deepcopy(oriImg)
+print(candidate)
+print(subset)
+canvas = util.draw_bodypose(oriImg, candidate, subset)
+
+plt.imshow(canvas[:, :, [2, 1, 0]])
+plt.axis('off')
+plt.show()
